19ee3a5c48ae80ba769985aaf04f2f0096a41c75
BastianMuc <nexus@mail11.de>
2023-08-06 10:43:02 +0200

    Branch switch

--- Bridge/UI/src/components/WalletConnect.vue
+++ Bridge/UI/src/components/WalletConnect.vue
@@ -1,25 +1,34 @@
 <template>
     <div id="connector" align="right">
         {{ walletStatus }}
+        <button disabled>{{ networkStatus }}</button>
         <button v-on:click="connectWallet">{{ checkMetamask }}</button>
     </div>
 </template>
 
 <script>
-import { ethers } from 'ethers'
+import { isProxy, markRaw, toRaw } from 'vue'
 export default {
+    inject: ['ethers'],
     data() {
         return {
            walletStatus: '',
+           networkStatus: '',
            connected: false,
-           provider: undefined,
+           provider: {},
            signer: undefined
         }
     },
     computed: {
         checkMetamask() {
             if (typeof window.ethereum !== undefined && this.connected === false) {
-                this.provider = new ethers.providers.Web3Provider(window.ethereum)
+                this.provider = new this.ethers.providers.Web3Provider(window.ethereum, "any")
+                this.provider.on("network", (newNetwork, oldNetwork) => {
+                    if (oldNetwork) {
+                        console.log("Event triggered!")
+                        window.location.reload()
+                    }
+                })
                 return 'Connect Wallet'
             } else if (this.connected === true) {
                 return 'Disconnect'
@@ -30,11 +39,37 @@ export default {
     },
     methods: {
         async connectWallet() {
-            const accounts = await this.provider.send('eth_requestAccounts', [])
-            //const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-            this.walletStatus = accounts[0]
-            this.signer = this.provider.getSigner()
-            if (this.walletStatus !== undefined) { this.connected = true }
+            if (!this.connected) {
+                const accounts = await this.provider.send('eth_requestAccounts', [])
+                //const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+                this.walletStatus = accounts[0]
+                //this.signer = this.provider.getSigner()
+                const network = await this.provider.getNetwork()
+                switch (network.chainId) {
+                    case 1:
+                        this.networkStatus = 'Ethereum'
+                        break
+                    case 5:
+                        this.networkStatus = 'Goerli'
+                        break
+                    case 324:
+                        this.networkStatus = 'zkSync Era'
+                        break
+                    case 280:
+                        this.networkStatus = 'zkSync Era Testnet'
+                        break
+                    default:
+                        this.networkStatus = 'Unsupported Network'
+                }
+                if (this.walletStatus !== undefined) { 
+                    this.connected = true
+                }
+            } else {
+                this.provider = undefined
+                this.walletStatus = ''
+                this.networkStatus = ''
+                this.connected = false
+            }
             return true
         }
     }
--- Bridge/UI/src/main.js
+++ Bridge/UI/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import { ethers } from 'ethers'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.provide('ethers', ethers)
+app.mount('#app')
+
+//createApp(App).mount('#app')
 
